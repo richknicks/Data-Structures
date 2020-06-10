bf9d8cccac6739dfa90d7294ff46620e8e052da8
richknicks <rlynch@gmail.com>
2020-06-10 19:46:42 -0400

    completed binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,20 +19,69 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check to see if is none, if none make head node
+        if self.value is None:
+            # assign to new node if none
+            self.value = value
+        # else if value equal or greater self.value place right
+        elif value >= self.value:
+            # check to see if right node is None
+            if self.right is None:
+                # assign to right node if true
+                self.right = BSTNode(value)
+                # else recursively move down one node and to the right
+            else:
+                self.right.insert(value)
+            # else place on the left
+        else:
+            # check to see if left node is None
+            if self.left is None:
+                # assign to left node if true
+                self.left = BSTNode(value)
+            # else recursively move down one node and to the left
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if base value is target return true
+        if self.value == target:
+            return True
+        # if self.value is greater than target
+        if self.value > target:
+            if self.left is not None:
+                # recursivley check down the left nodes
+                return self.left.contains(target)
+        elif self.value < target:
+            if self.right is not None:
+                # recursivley check down the right nodes
+                return self.right.contains(target)
+            # create a catch for end of list to return False
+        else:
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # recurse until none
+        if not self.right:
+            return self.value
+        else:
+            # else recursively move through right nodes
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call fn on the value of the node
+        fn(self.value)
+        # check if left node exists
+        if self.left:
+            # recursively move down through left nodes passing down function
+            self.left.for_each(fn)
+        # check if right node exists
+        if self.right:
+            # recursively move down through right nodes passing down function
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
